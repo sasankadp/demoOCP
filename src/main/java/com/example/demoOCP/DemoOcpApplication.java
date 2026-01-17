d6d86009fa93ea5a6ceb11a5dd4ca8eb0847e4a3
sasankadp <sasankadp14@gmail.com>
2026-01-17 11:08:32 -0500

    first commit

---
+++ src/main/java/com/example/demoOCP/DemoOcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoOCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoOCP/DemoOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoOCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
