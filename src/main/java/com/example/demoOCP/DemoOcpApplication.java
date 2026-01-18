07d433690c6d92499528aca0d367e5217372a692
sasankadp <sasankadp14@gmail.com>
2026-01-18 16:17:37 -0500

    postconstruct issue intentional error

--- src/main/java/com/example/demoOCP/DemoOcpApplication.java
+++ src/main/java/com/example/demoOCP/DemoOcpApplication.java
@@ -1,5 +1,6 @@
 package com.example.demoOCP;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class DemoOcpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOcpApplication.class, args);
+	}
+
+	@PostConstruct
+	public void crashOnStepup(){
+		throw new RuntimeException("Intentional crash for CrashLoopBackOf");
 	}
 
 }
