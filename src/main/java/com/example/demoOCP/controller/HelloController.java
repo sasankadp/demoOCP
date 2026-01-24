2aca0f423588705802181a36813e4c5e6326f262
sasankadp <sasankadp14@gmail.com>
2026-01-24 14:46:32 -0500

    OCP yaml push automation test

--- src/main/java/com/example/demoOCP/controller/HelloController.java
+++ src/main/java/com/example/demoOCP/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String Hello(){
-        return "Hello returned with automation deployment testing1";
+        return "Hello returned with automation deployment testing2";
     }
 }
